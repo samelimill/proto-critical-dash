c04021c519b8c89a399d198359ae0c17bc67713f
samelimill <samelimill@gmail.com>
2023-08-13 14:28:11 -0700

    Added basic HTML framework

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+var start=document.getElementById('start');
+var quizContainer=document.getElementByID('quiz-container');
+var question=document.getElementByID('question');
+var answerContainer=document.getElementByID('answers');
+var answerA=document.getElementByID('A');
+var answerB=document.getElementByID('B');
+var answerC=document.getElementByID('C');
+var answerD=document.getElementByID('D');
+
+let questions = [
+    {question:,
+    A:,
+    B:,
+    C:,
+    D:,
+    correct:,},
+    {question:,
+    A:,
+    B:,
+    C:,
+    D:,
+    correct:,},
+    {question:,
+    A:,
+    B:,
+    C:,
+    D:,
+    correct:,},
+    {question:,
+    A:,
+    B:,
+    C:,
+    D:,
+    correct:,}, 
+    
+]
+
+start.addEventListener('click', runQuiz());
+function runQuiz(
+)
+
